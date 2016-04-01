4200fc99d30e559f5a143d68e41ffb1eece65570
mishachris <chrissk@mail.ru>
2016-04-01 14:16:18 +0300

    Update and rename Sum.java to Zarplata.java

---
+++ Zarplata.java
@@ -0,0 +1,11 @@
+
+public class Zarplata {
+	
+	 public static void main(String[] args) {
+		 
+			int z = 35_000;
+			int nalog = 13;
+			int p = 35000 * (100 - nalog)/100;
+			System.out.println("Моя зарплата - " + p + " рублей.");
+		}
+}
